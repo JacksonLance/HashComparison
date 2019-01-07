071b125693cca3782e08ecf6df673978565c2f13
JacksonLance <46432925+JacksonLance@users.noreply.github.com>
2019-01-06 22:18:03 -0500

    Add files via upload

---
+++ HashingPairs/src/HP.java
@@ -0,0 +1,83 @@
+import javax.xml.bind.DatatypeConverter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HP {
+
+    public static String ToSHA256(byte[] convertSHA) {
+        MessageDigest md = null;
+        String hashtext = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            byte[] message = md.digest(convertSHA);
+            BigInteger no = new BigInteger(1, message);
+            hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hashtext;
+    }
+    public static String ToSHA512(byte[] convertSHA) {
+        MessageDigest md = null;
+        String hashtext = null;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+            byte[] message = md.digest(convertSHA);
+            BigInteger no = new BigInteger(1, message);
+            hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hashtext;
+    }
+    public static Boolean Compare(String s, String two)
+    {
+        Boolean b = false;
+        if (s.compareTo(two) == 0)
+        {
+            b = true;
+        }
+        return b;
+    }
+    public static Boolean CompareSHA256(String s, String two)
+    {
+        return Compare(ToSHA256(s.getBytes()),ToSHA256(two.getBytes()));
+
+    }
+    public static Boolean CompareSHA512(String s, String two)
+    {
+        return Compare(ToSHA512(s.getBytes()),ToSHA512(two.getBytes()));
+
+    }
+    public static Boolean CompareBooleans(String s, String two)
+    {
+        return (CompareSHA256(s, two) == CompareSHA512(s,two));
+    }
+    public static void main (String args[])
+    {
+
+        System.out.println(ToSHA256("Thursday".getBytes()));
+        System.out.println(ToSHA256("Thursday".getBytes()));
+        System.out.println(CompareSHA256("Thursday", "Thursday"));
+        System.out.println(ToSHA256("thursday".getBytes()));
+        System.out.println(CompareSHA256("Thursday", "thursday"));
+        System.out.println(ToSHA512("Thursday".getBytes()));
+        System.out.println(ToSHA512("thursday".getBytes()));
+        System.out.println(CompareSHA512("Thursday", "thursday"));
+        System.out.println(CompareBooleans("Thursday", "thursday"));
+
+
+
+
+    }
+
+}
